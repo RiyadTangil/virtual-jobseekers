f46f452c5228ece8166cf0acf88e55bdfc68bb49
Accountant-riyad <Accountant-riyad>
2021-08-21 20:04:47 +0600

    first commit

---
+++ src/Components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBjXpBCb9gG_8Yk-ufYA3chlKR9DkuCJuE",
+  authDomain: "virtual-jobseekers.firebaseapp.com",
+  projectId: "virtual-jobseekers",
+  storageBucket: "virtual-jobseekers.appspot.com",
+  messagingSenderId: "232179221226",
+  appId: "1:232179221226:web:f89ac1730ec109994d8d93"
+  };
+  export default firebaseConfig;
